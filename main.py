6eb76b41a14ed67d11fbc6f22806c459399835e6
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-25 09:23:54 -0300

    testing player inheritance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from objects.characters import Character
+from objects.characters import Player, Enemy
 #from objects.inventory import Inventory
 from objects.starting_menu import StartingMenu
 
-x = StartingMenu()
-x
+player = Player('Player', 100, 20)
+player.set_health(120)
+print(player.get_health())
