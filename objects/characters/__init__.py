215c3cb5eecbe0a4eb13bdc0275ae0b5d0246b7e
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-25 09:00:34 -0300

    created class Player

--- objects/characters/__init__.py
+++ objects/characters/__init__.py
@@ -1,6 +1,10 @@
 class Character:
     def __init__(self, name: str, health: int, attack: int) -> None:
         self.name = name
-        self.health = health
-        self.attack = attack
+        self.__health = health
+        self.__attack = attack
+
+class Player(Character):
+    def __init__(self, name, health, attack):
+        super().__init__(name = 'Player', health = 100, attack = 20)
 
