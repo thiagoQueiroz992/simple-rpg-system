4e8169a707ed204000ac240b786dbd0e5262cc86
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-25 09:15:28 -0300

    new_game_button created

--- objects/starting_menu/__init__.py
+++ objects/starting_menu/__init__.py
@@ -4,6 +4,8 @@ from rich import print
 
 class StartingMenu:
     game_panel = Panel(Align('Oneihl Kingdom Adventure', 'center'))
+    new_game_button = Panel(Align('Press Enter to start a new game', 'center'), width=40)
 
     def __init__(self):
         print(self.game_panel)
+        print(self.new_game_button)
