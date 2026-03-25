fd41e54ee36ec50b88461356fd4c6215db60251d
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-25 09:19:31 -0300

    using getters and setters on Character

--- objects/characters/__init__.py
+++ objects/characters/__init__.py
@@ -3,6 +3,13 @@ class Character:
         self.name = name
         self.__health = health
         self.__attack = attack
+    
+    def get_health(self) -> int:
+        return self.__health
+    
+    def set_health(self, value: int) -> None:
+        if self.__health > 0:
+            self.__health = value
 
 class Player(Character):
     def __init__(self, name, health, attack):
