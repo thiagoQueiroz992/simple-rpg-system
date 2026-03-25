e8b7fb7cc29c2439845aafd72e54bab9e20c58d8
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-25 09:11:08 -0300

    first instance of StartingMenu and some adjusts

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from objects.characters import Character
 #from objects.inventory import Inventory
 from objects.starting_menu import StartingMenu
 
+x = StartingMenu()
+x
--- objects/starting_menu/__init__.py
+++ objects/starting_menu/__init__.py
@@ -1,7 +1,9 @@
 from rich.panel import Panel
+from rich.align import Align
+from rich import print
 
 class StartingMenu:
-    game_panel = Panel('Oneihl Kingdom Adventure')
+    game_panel = Panel(Align('Oneihl Kingdom Adventure', 'center'))
 
     def __init__(self):
         print(self.game_panel)
