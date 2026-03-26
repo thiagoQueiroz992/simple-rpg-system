c93ff17c76477652363a6ed489ce9df915c0e96c
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-26 09:02:12 -0300

    Character class created

---
+++ characters.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, name: str, health: int, attack: int):
+        self.name = name
+        self.__health = health
+        self.__attack = attack
+    
+    def get_health(self) -> int:
+        return self.__health
+    
+    def set_health(self, value: int):
+        if value > 0:
+            self.__health = value
