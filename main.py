478ba435aeeb47fa503c1bc50d09a74dcdec8a3e
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-26 08:57:08 -0300

    restarting everything

--- main.py
+++
@@ -1,6 +0,0 @@
-from objects.characters import Player, Enemy
-#from objects.inventory import Inventory
-from objects.starting_menu import StartingMenu
-
-player = Player('Player', 100, 20)
-player.move()
--- objects/events/__init__.py
+++
@@ -1,5 +0,0 @@
-from objects.characters import Enemy
-
-def spawn_enemy(name: str, health: int, attack: int) -> Enemy:
-    enemy = Enemy(name, health, attack)
-    return enemy
