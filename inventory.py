d17966c437d878c62040dc35d671a4b02587b362
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-30 08:50:05 -0300

    inner class Item created

--- inventory.py
+++ inventory.py
@@ -3,3 +3,9 @@ class Inventory:
     
     def __init__(self):
         self.__items = []
+
+    class Item:
+        stack_max_item_quant = 16
+
+        def __init__(self, stack_item_quant: int = 1):
+            self.__stack_item_quant = stack_item_quant
