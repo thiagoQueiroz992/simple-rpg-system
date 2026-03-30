14086092eb9ffe602634a108f6405dfbdb439fe2
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-03-30 14:41:49 -0300

    created class Item

--- inventory.py
+++ inventory.py
@@ -1,11 +1,14 @@
+from rich import inspect
+
 class Inventory:
     slots = 10
     
     def __init__(self):
         self.__items = []
 
-    class Item:
-        stack_max_item_quant = 16
 
-        def __init__(self, stack_item_quant: int = 1):
-            self.__stack_item_quant = stack_item_quant
+class Item:
+    stack_max_items = 16
+
+    def __init__(self, amount: int = 1):
+        self.amount = amount
