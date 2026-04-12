a978eedea7c9ce1f56191d0626a1b3b80eca2550
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-04-12 11:14:36 -0300

    starting menu enhanced

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from rich import inspect, print
 from rich.panel import Panel
 from rich.align import Align
 from os import system
+from time import sleep
 from options import Question
 
 system('cls')
 
 print(Panel(Align('ONEIHL\'S RPG GAME', align='center', vertical='middle'), subtitle='A RPG style game made for studying purpose', style='bold green', height=5))
+sleep(2)
 print()
 
 player = characters.Player(Question('Enter your name:', '-', question_type='text').show_question(), 100, 20)
+print(Panel(Align(f'Welcome [cyan]{player.name}[/cyan]! You are going to start your new adventure!', align='center'), style='green bold'))
+sleep(3.5)
 player.idle()
